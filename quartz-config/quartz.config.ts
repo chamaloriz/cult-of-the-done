c0d452b3901fd68a88338fe9787c8cb9f627aab0
Radislav Myasnikov <radislav@surleweb.be>
2025-09-20 15:58:30 +0200

    changed website name to 3ronds.net

--- quartz-config/quartz.config.ts
+++ quartz-config/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Cult of the done",
+    pageTitle: "3ronds.net",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
